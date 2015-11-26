446713d2e541e6a9baf2c3307344a651a95c0e50
unknown <DTimur@Timur-LT.mtfdev.com>
2015-11-26 16:17:12 +0500

    добавление новой стриточки с выводом инфы на экран

--- VS-project/Program.cs
+++ VS-project/Program.cs
@@ -11,6 +11,8 @@ namespace VS_project
         static void Main(string[] args)
         {
             Console.WriteLine("Вывод текста на экран");
+
+            Console.WriteLine("Изменения в коде");
         }
     }
 }
